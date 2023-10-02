a3e146c208735db9f07fd88ce39b6d071d777809
PhoonThetPine <thetpine254@gmail.com>
2023-10-02 09:57:59 +0800

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Phoon Thet Pine on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
